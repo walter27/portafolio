78fbb91abc194ade77057376c84f9e30d20dfaa8
walter_rz <wremache@uazuay.edu.ec>
2022-06-20 10:28:21 -0500

    Correcion envio correo client_id

--- public/js/contact.js
+++ public/js/contact.js
@@ -4,9 +4,7 @@ let mail = document.getElementById('email');
 let message = document.getElementById('message');
 let contacto = document.getElementById('contacto');
 let mensaje = document.createElement('h4');
-mensaje.classList.add('message');
 mensaje.setAttribute('id', 'mensaje');
-mensaje.append('Correo enviado');
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -23,11 +21,16 @@ contactForm.addEventListener('submit', (e) => {
             names.value = "";
             mail.value = ""
             message.value = "";
-            contacto.after(mensaje);
-            setTimeout(() => {
-                document.getElementById('mensaje').remove();
-            }, 2000);
+            mensaje.classList.add('message');
+            mensaje.append('Correo enviado');
+        } else {
+            mensaje.classList.add('message-error');
+            mensaje.append('Error interno');
         };
+        contacto.after(mensaje);
+        setTimeout(() => {
+            document.getElementById('mensaje').remove();
+        }, 2000);
     };
     xhr.send(JSON.stringify(formData));
 });
